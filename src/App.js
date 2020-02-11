0c9845af6416ec6cf07d5d4ecce8dcd10838a017
Eric Wood <epwood1@gmail.com>
2020-02-10 18:58:41 -0800

    MVP submit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DogsList from "./components/dogsList";
+
+function App() {
+  return (
+    <div className="App">
+      <DogsList/>
+    </div>
+  );
+}
+
+export default App;
